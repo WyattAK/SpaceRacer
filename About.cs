249734ba6de3404716965931ae12946de2f71cd0
Wyatt <82850335+WyattAK@users.noreply.github.com>
2021-04-19 20:18:31 -0400

    Space Racer C# Scripts
    All C# scripts used in the making of the game Space Racer

---
+++ About.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class About : MonoBehaviour {
+
+	public void BackToMenu () {
+		//Load "Menu" Scene
+		SceneManager.LoadScene ("Menu");
+	}
+
+}
